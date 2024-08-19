6a845d98506b0493dfc4fe5ec7905cd8e466c6bb
Max Farsikov <farsikovmax@gmail.com>
2024-08-20 00:18:54 +0300

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,58 @@
 import com.github.mfarsikov.kewt.versioning.plugin.Incrementer
 
 plugins {
-    `java-gradle-plugin`
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("com.gradle.plugin-publish") version "0.14.0"
-    id("maven-publish")
-    id("com.github.mfarsikov.kewt-versioning") version "1.0.0"
+  `java-gradle-plugin`
+  id("org.jetbrains.kotlin.jvm") version "2.0.10"
+  id("com.gradle.plugin-publish") version "1.2.1"
+  id("maven-publish")
+  id("com.github.mfarsikov.kewt-versioning") version "1.0.0"
 }
 
 group = "com.github.mfarsikov.kewt-versioning"
 kewtVersioning {
-    configuration {
-        branches {
-            clear()
-            add {
-                regexes = mutableListOf("master".toRegex())
-                incrementer = Incrementer.MINOR
-                stringify = stringifier(useBranch = false, useSha = false, useTimestamp = false)
-            }
-            add {
-                regexes = mutableListOf("fix/.*".toRegex())
-                incrementer = Incrementer.PATCH
-                stringify = stringifier(useSha = false, useTimestamp = false)
-            }
-            add {
-                regexes = mutableListOf(".*".toRegex())
-                incrementer = Incrementer.MINOR
-                stringify = { version ->
-                    stringifier(
-                        useBranch = version.isSnapshot,
-                        useSha = false,
-                        useTimestamp = false
-                    )(version)
-                }
-            }
+  configuration {
+    branches {
+      clear()
+      add {
+        regexes = mutableListOf("master".toRegex())
+        incrementer = Incrementer.MINOR
+        stringify = stringifier(useBranch = false, useSha = false, useTimestamp = false)
+      }
+      add {
+        regexes = mutableListOf("fix/.*".toRegex())
+        incrementer = Incrementer.PATCH
+        stringify = stringifier(useSha = false, useTimestamp = false)
+      }
+      add {
+        regexes = mutableListOf(".*".toRegex())
+        incrementer = Incrementer.MINOR
+        stringify = { version ->
+          stringifier(
+            useBranch = version.isSnapshot,
+            useSha = false,
+            useTimestamp = false
+          )(version)
         }
+      }
     }
+  }
 }
 version = kewtVersioning.version
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.12.0.202106070339-r")
+  implementation("org.eclipse.jgit:org.eclipse.jgit:6.10.0.202406032230-r")
 }
 
 gradlePlugin {
-    val kewtVersioning by plugins.creating {
-        id = "com.github.mfarsikov.kewt-versioning"
-        displayName = "Kewt versioning"
-        implementationClass = "com.github.mfarsikov.kewt.versioning.plugin.KewtVersioningPlugin"
-    }
+  val kewtVersioning by plugins.creating {
+    id = "com.github.mfarsikov.kewt-versioning"
+    displayName = "Kewt versioning"
+    implementationClass = "com.github.mfarsikov.kewt.versioning.plugin.KewtVersioningPlugin"
+  }
 }
 
 val functionalTestSourceSet = sourceSets.create("functionalTest") {
@@ -62,43 +62,49 @@ gradlePlugin.testSourceSets(functionalTestSourceSet)
 configurations.getByName("functionalTestImplementation").extendsFrom(configurations.getByName("testImplementation"))
 
 val functionalTest by tasks.creating(Test::class) {
-    testClassesDirs = functionalTestSourceSet.output.classesDirs
-    classpath = functionalTestSourceSet.runtimeClasspath
+  testClassesDirs = functionalTestSourceSet.output.classesDirs
+  classpath = functionalTestSourceSet.runtimeClasspath
 }
 
 val check by tasks.getting(Task::class) {
-    dependsOn(functionalTest)
+  dependsOn(functionalTest)
 }
 
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group as String
-            artifactId = project.name
-            version = project.version.toString()
-            from(components["java"])
-        }
+  publications {
+    create<MavenPublication>("maven") {
+      groupId = project.group as String
+      artifactId = project.name
+      version = project.version.toString()
+      from(components["java"])
     }
+  }
 }
 
 kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
+  jvmToolchain {
+    languageVersion.set(JavaLanguageVersion.of(21))
+  }
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
+kotlin {
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
 }
 
-pluginBundle {
-    website = "https://github.com/mfarsikov/kewt-versioning"
-    vcsUrl = "https://github.com/mfarsikov/kewt-versioning"
-    description = "Gradle plugin for versioning using Git tags"
-    tags = listOf("git", "versioning")
+gradlePlugin {
+  website = "https://github.com/mfarsikov/kewt-versioning"
+  vcsUrl = "https://github.com/mfarsikov/kewt-versioning"
+  plugins {
+    matching { it.name == "kewtVersioning" }.configureEach {
+      id = "com.github.mfarsikov.kewt-versioning"
+      displayName = "Kewt versioning"
+      description = "Gradle plugin for versioning using Git tags"
+      tags = listOf("git", "versioning")
+      implementationClass = "com.github.mfarsikov.kewt.versioning.plugin.KewtVersioningPlugin"
+    }
+  }
 }
 project.ext["gradle.publish.key"] = System.getenv("GRADLE_PUBLISH_KEY")
 project.ext["gradle.publish.secret"] = System.getenv("GRADLE_PUBLISH_SECRET")
